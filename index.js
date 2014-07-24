214ee9eef621bf3efb9ee39103a345a14f8d5231
Anson Yu Wang <opensource@undozen.com>
2014-07-25 06:51:46 +0800

    support partials and sub-components

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ var reactTools = require('react-tools');
 module.exports = function (file) {
   return through(function (buf, enc, next) {
     if (file.match(/\.htmlx$/i)) {
-      this.push(reactTools.transform('/**\n'+
-        '* @jsx React.DOM\n'+
-        '*/\n'+
-        'var React = require("react");module.exports = function () {\n'+
-        '  return '+buf.toString('utf8')+'};'
-      , {filename: file}));
+      var htmlx = reactTools.transform(
+        '/** @jsx React.DOM */ function htmlx(ctx, state, props) { '+buf.toString('utf8')+'};'
+      , {filename: file});
+      var exp = 'var React = require("react"); module.exports = HTMLX; function HTMLX(ctx) { return function () { return htmlx.call(ctx, ctx, ctx.state, ctx.props); '+htmlx+'}}'
+      this.push(exp);
     } else {
       this.push(buf);
     }
