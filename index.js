1e2c68644a8136103b930d6183dc82f1ee789fd3
Anson Yu Wang <opensource@undozen.com>
2014-07-29 23:53:57 +0800

    use strict to prevent global variable leak when require partials or components

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (file) {
   return through(function (buf, enc, next) {
     if (file.match(/\.htmlx$/i)) {
       var htmlx = reactTools.transform(
-        '/** @jsx React.DOM */ function htmlx(ctx, state, props) { '+buf.toString('utf8')+'};'
+        '/** @jsx React.DOM */ function htmlx(ctx, state, props) { "use strict"; '+buf.toString('utf8')+'};'
       , {filename: file});
       var exp = 'var React = require("react"); module.exports = HTMLX; function HTMLX(ctx) { return function () { return htmlx.call(ctx, ctx, ctx.state, ctx.props); '+htmlx+'}}'
       this.push(exp);
